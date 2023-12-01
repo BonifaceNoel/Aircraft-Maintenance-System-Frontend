401e42bce797bf08f8cf433b513c607c3c03d216
BonifaceNoel <bonifacemac658@gmail.com>
2023-12-01 10:06:20 +0530

    new-entry-component-styling-commit

--- VSCode-React-MUI/src/APIComponents/NewEntry.jsx
+++ VSCode-React-MUI/src/APIComponents/NewEntry.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import NewFlight from './NewFlight';
 import NewMaintenance from './NewMaintenance';
+import backgroundImg from '../../public/flight-2.jpg';
 
 const NewEntry = () => {
     const [flight, setFlight] = useState(false);
@@ -24,11 +25,16 @@ const NewEntry = () => {
     };
 
     return (
-        <div>
-            <Button variant="contained" onClick={flightOpen} style={{marginLeft: '20px', marginTop: '-20rem'}}>
+        <div style={{}}>
+            <img
+                src={backgroundImg}
+                alt="Description of the image"
+                style={{ width: '100%', height: '47rem' }}
+            />
+            <Button variant="contained" onClick={flightOpen} style={{ marginLeft: '20px', marginTop: '-70rem' }}>
                 Open New Flight Form
             </Button>
-            <Button variant="contained" onClick={maintenanceOpen} style={{ marginLeft: '20px', marginTop: '-20rem' }}>
+            <Button variant="contained" onClick={maintenanceOpen} style={{ marginLeft: '20px', marginTop: '-70rem' }}>
                 Open New maintenance Form
             </Button>
             <NewFlight open={flight} onClose={flightClose}></NewFlight>
